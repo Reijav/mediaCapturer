dedeeacb35b0b1c3ba3c71bd5f4506766f2f7238
Javier Pinto <jpinto@vimeworks.com>
2020-03-04 08:27:25 -0500

    inicio proyecto

---
+++ MediaCapturer/CameraCapturer/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CameraCapturer
+{
+    partial class FormCapturador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxDispositivos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonObtenerVideo = new System.Windows.Forms.Button();
+            this.buttonCapturarImg = new System.Windows.Forms.Button();
+            this.buttonGrabar = new System.Windows.Forms.Button();
+            this.imageListCaptured = new System.Windows.Forms.ImageList(this.components);
+            this.listViewImages = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 331);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBoxDispositivos
+            // 
+            this.comboBoxDispositivos.FormattingEnabled = true;
+            this.comboBoxDispositivos.Location = new System.Drawing.Point(103, 12);
+            this.comboBoxDispositivos.Name = "comboBoxDispositivos";
+            this.comboBoxDispositivos.Size = new System.Drawing.Size(271, 21);
+            this.comboBoxDispositivos.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dispositivos";
+            // 
+            // buttonObtenerVideo
+            // 
+            this.buttonObtenerVideo.Location = new System.Drawing.Point(16, 43);
+            this.buttonObtenerVideo.Name = "buttonObtenerVideo";
+            this.buttonObtenerVideo.Size = new System.Drawing.Size(154, 23);
+            this.buttonObtenerVideo.TabIndex = 4;
+            this.buttonObtenerVideo.Text = "Conectar Con Dispositivo";
+            this.buttonObtenerVideo.UseVisualStyleBackColor = true;
+            this.buttonObtenerVideo.Click += new System.EventHandler(this.buttonConectarDispositivo_Click);
+            // 
+            // buttonCapturarImg
+            // 
+            this.buttonCapturarImg.Location = new System.Drawing.Point(586, 43);
+            this.buttonCapturarImg.Name = "buttonCapturarImg";
+            this.buttonCapturarImg.Size = new System.Drawing.Size(130, 23);
+            this.buttonCapturarImg.TabIndex = 5;
+            this.buttonCapturarImg.Text = "Capturar Imagen";
+            this.buttonCapturarImg.UseVisualStyleBackColor = true;
+            this.buttonCapturarImg.Click += new System.EventHandler(this.buttonCapturarImg_Click_1);
+            // 
+            // buttonGrabar
+            // 
+            this.buttonGrabar.Location = new System.Drawing.Point(310, 43);
+            this.buttonGrabar.Name = "buttonGrabar";
+            this.buttonGrabar.Size = new System.Drawing.Size(118, 23);
+            this.buttonGrabar.TabIndex = 6;
+            this.buttonGrabar.Text = "Grabar Video";
+            this.buttonGrabar.UseVisualStyleBackColor = true;
+            this.buttonGrabar.Click += new System.EventHandler(this.buttonGrabarVideo_Click);
+            // 
+            // imageListCaptured
+            // 
+            this.imageListCaptured.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListCaptured.ImageSize = new System.Drawing.Size(128, 128);
+            this.imageListCaptured.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listViewImages
+            // 
+            this.listViewImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewImages.LargeImageList = this.imageListCaptured;
+            this.listViewImages.Location = new System.Drawing.Point(448, 72);
+            this.listViewImages.Name = "listViewImages";
+            this.listViewImages.Size = new System.Drawing.Size(268, 331);
+            this.listViewImages.SmallImageList = this.imageListCaptured;
+            this.listViewImages.TabIndex = 7;
+            this.listViewImages.UseCompatibleStateImageBehavior = false;
+            // 
+            // FormCapturador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 415);
+            this.Controls.Add(this.listViewImages);
+            this.Controls.Add(this.buttonGrabar);
+            this.Controls.Add(this.buttonCapturarImg);
+            this.Controls.Add(this.buttonObtenerVideo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxDispositivos);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormCapturador";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormCapturador_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxDispositivos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonObtenerVideo;
+        private System.Windows.Forms.Button buttonCapturarImg;
+        private System.Windows.Forms.Button buttonGrabar;
+        private System.Windows.Forms.ImageList imageListCaptured;
+        private System.Windows.Forms.ListView listViewImages;
+    }
+}
+
---
+++ MediaCapturer/CameraCapturer/Form1.cs
@@ -0,0 +1,187 @@
+﻿using Accord.Video.FFMPEG;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CameraCapturer
+{
+    public partial class FormCapturador : Form
+    {
+
+        private string path = @"C:\temp\capturador\";
+        private FilterInfoCollection MisDispositivos;
+        private VideoCaptureDevice MiWebCam;
+
+
+        private VideoFileWriter FileWriter = new VideoFileWriter();
+        private SaveFileDialog saveAvi;
+        private Bitmap Imagen;
+
+        private const string PARAR_GRABAR = "Parar Grabación";
+        private const string GRABAR_VIDEO = "Grabar Video";
+        private const string DESCONECTAR = "Desconectar de Dispositivo";
+
+        public FormCapturador()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDispositivos();
+        }
+
+        public void CargarDispositivos()
+        {
+            MisDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
+            if(MisDispositivos!=null && MisDispositivos.Count > 0)
+            {
+                foreach(FilterInfo dispositivo in MisDispositivos)
+                {
+                    
+                    comboBoxDispositivos.Items.Add(dispositivo.Name);
+                }
+                comboBoxDispositivos.Text = MisDispositivos[0].Name;
+            }
+
+        }
+
+        private long numeroPrevio ;
+
+        private void CapturandoImagen(object sender , NewFrameEventArgs newFrameEventArgs)
+        {
+            long numeroActual = DateTime.Now.Ticks;
+            Imagen = (Bitmap)newFrameEventArgs.Frame.Clone();
+            pictureBox1.Image = (Bitmap)newFrameEventArgs.Frame.Clone();
+
+            //SI SE ENCUENTRA GRABANDO
+            if (buttonGrabar.Text == PARAR_GRABAR)
+            {
+                var lapsoTiempo = numeroActual - numeroPrevio;
+                var lapsoTiempoTS = new TimeSpan(numeroActual - numeroPrevio);
+
+                if (lapsoTiempoTS.TotalSeconds > 1)
+                {
+                    FileWriter.WriteVideoFrame(Imagen, lapsoTiempoTS);
+                }
+                else
+                {
+                    FileWriter.WriteVideoFrame(Imagen);
+                }
+
+                
+            }
+           // numeroPrevio = numeroActual;
+        }
+
+        private void CerrarWebCam()
+        {
+            buttonGrabar.Text = GRABAR_VIDEO;
+            Task.Delay(100); 
+            if (MiWebCam!=null && MiWebCam.IsRunning)
+            {
+                if(FileWriter!=null && FileWriter.IsOpen)
+                {
+                    FileWriter.Close();
+                }
+
+
+                MiWebCam.SignalToStop();
+                MiWebCam = null;
+                pictureBox1.Image = null;
+            }
+            
+        }
+
+        private void FormCapturador_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CerrarWebCam();
+        }
+
+        private void buttonGrabarVideo_Click(object sender, EventArgs e)
+        {
+
+
+            if (buttonGrabar.Text == PARAR_GRABAR)
+            {
+                buttonGrabar.Text = GRABAR_VIDEO;
+                if (MiWebCam == null)
+                { return; }
+                if (MiWebCam.IsRunning)
+                {
+                    //this.FinalVideo.Stop();
+                    FileWriter.Close();
+                    //this.AVIwriter.Close();
+                   // pictureBox1.Image = null;
+                }
+            }
+            else
+            {
+                if(MiWebCam!=null && MiWebCam.IsRunning)
+                {
+                    saveAvi = new SaveFileDialog();
+                    saveAvi.Filter = "Avi Files (*.avi)|*.avi";
+                    if (saveAvi.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        numeroPrevio = DateTime.Now.Ticks;
+                        int h = MiWebCam.VideoResolution.FrameSize.Height;
+                        int w = MiWebCam.VideoResolution.FrameSize.Width;
+                        FileWriter.Open(saveAvi.FileName, w, h, 25, VideoCodec.Default, 5000000);
+                        FileWriter.WriteVideoFrame(Imagen);
+                        buttonGrabar.Text = PARAR_GRABAR;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No hay conexión a ningún dispositivo de video.");
+                }
+
+            }
+        }
+
+        private void buttonCapturarImg_Click_1(object sender, EventArgs e)
+        {
+            Bitmap imagenCapturada= Imagen;
+            string nombreArchivo = $"{path}{DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss")}.jpg";
+            imagenCapturada.Save(nombreArchivo, System.Drawing.Imaging.ImageFormat.Jpeg);
+            imageListCaptured.Images.Add(imagenCapturada);
+            listViewImages.Refresh();
+            listViewImages.Items.Add(nombreArchivo, listViewImages.Items.Count);
+
+        }
+
+        private void buttonConectarDispositivo_Click(object sender, EventArgs e)
+        {
+            CerrarWebCam();
+            if (buttonObtenerVideo.Text != DESCONECTAR)
+            {
+                if (comboBoxDispositivos.SelectedIndex >= 0)
+                {
+                    buttonObtenerVideo.Text = DESCONECTAR;
+                    int indice = comboBoxDispositivos.SelectedIndex;
+                    string nombreVideo = MisDispositivos[indice].MonikerString;
+                    MiWebCam = new VideoCaptureDevice(nombreVideo);
+                    MiWebCam.VideoResolution = MiWebCam.VideoCapabilities[0];
+                    MiWebCam.NewFrame += new NewFrameEventHandler(CapturandoImagen);
+                    MiWebCam.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Debe seleccionar un dispositivo");
+                }
+
+            }
+            else
+            {
+                buttonObtenerVideo.Text = "Conectar con dispositivo";
+            }
+
+        }
+    }
+}
